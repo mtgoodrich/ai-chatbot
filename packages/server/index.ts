41146c07c13c1f8a2fd8138a5072644157c6996c
Matthew Goodrich <matthew@propertylens.com>
2025-08-22 17:00:05 -0700

    Connect the frontend and backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -11,6 +11,10 @@ app.get("/", (req: Request, res: Response) => {
     res.send("AI App Server");
 });
 
+app.get("/api/hello", (req: Request, res: Response) => {
+    res.json({ message: "hello world" });
+});
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
