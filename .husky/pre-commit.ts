26f112906a6909d9aaa20c209e3b766625596f61
Matthew Goodrich <matthew@propertylens.com>
2025-08-22 19:49:16 -0700

    Stage Husky

---
+++ .husky/pre-commit.ts
@@ -0,0 +1,3 @@
+// remove .ts extension to run lint-staged files format with prettier from command line
+// use npx if committing from source control panel
+// bunx lint-staged
---
+++ .lintstagedrc.ts
@@ -0,0 +1,4 @@
+// remove .ts extension to format on save with prettier
+// {
+//     "*.{js,jsx,ts,tsx,css}": "prettier --write"
+// }
