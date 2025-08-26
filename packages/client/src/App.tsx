ae544f652169d4318b1ffb4ee5a279d1c407d1cc
Matthew Goodrich <matthew@propertylens.com>
2025-08-26 13:14:52 -0700

    Add frontend input and messages components

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,18 +1,9 @@
-import { useEffect, useState } from "react";
-import { Button } from "./components/ui/button";
+import ChatBot from "./components/chat/ChatBot";
 
 function App() {
-    const [message, setMessage] = useState("");
-
-    useEffect(() => {
-        fetch("/api/hello")
-            .then((res) => res.json())
-            .then((data) => setMessage(data.message));
-    }, []);
     return (
-        <div className="p-4">
-            <p className="font-bold py-4 text-3xl">{message}</p>
-            <Button>Click</Button>
+        <div className="p-4 h-screen">
+            <ChatBot />
         </div>
     );
 }
---
+++ packages/client/src/components/chat/ChatInput.tsx
@@ -0,0 +1,55 @@
+import { useForm } from "react-hook-form";
+import { FaArrowUp } from "react-icons/fa";
+
+import { Button } from "../ui/button";
+
+export type ChatFormData = {
+    prompt: string;
+};
+
+type Props = {
+    onSubmit: (data: ChatFormData) => void;
+};
+
+const ChatInput = ({ onSubmit }: Props) => {
+    const { register, handleSubmit, formState, reset } =
+        useForm<ChatFormData>();
+
+    const handleFormSubmit = handleSubmit((data) => {
+        reset({ prompt: "" });
+        onSubmit(data);
+    });
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLFormElement>) => {
+        if (e.key === "Enter" && !e.shiftKey) {
+            e.preventDefault();
+            handleFormSubmit();
+        }
+    };
+
+    return (
+        <form
+            onSubmit={handleFormSubmit}
+            onKeyDown={handleKeyDown}
+            className="p-4 flex flex-col gap-2 items-end border-2 rounded-3xl"
+        >
+            <textarea
+                {...register("prompt", {
+                    required: true,
+                    validate: (data) => data.trim().length > 0,
+                })}
+                className="w-full border-0 focus:outline-0 resize-none"
+                placeholder="Ask anything"
+                maxLength={1000}
+            />
+            <Button
+                className="rounded-full w-9 h-9f"
+                disabled={formState.isValid == false}
+            >
+                <FaArrowUp />
+            </Button>
+        </form>
+    );
+};
+
+export default ChatInput;
